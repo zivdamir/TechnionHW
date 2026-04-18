ae2a4f7a08405f5d0665ca788cf7d41aee4ee1bf
zivdamir <73081641+zivdamir@users.noreply.github.com>
2026-04-18 11:52:49 +0300

    changing character.h to make more sense c++ wise..

--- Character.h
+++ Character.h
@@ -10,30 +10,31 @@
 namespace mtm {
     class Character {
     protected:
-        int health;
-        int ammo;
-        int range;
-        int power;
-        Team team;
-        CharacterType type;
+        int health; // A threshold to the amount of hit points a unit may take before it no longer is able to participate in the game.
+        int ammo;  // An upper bound to the amount of special attacks a unit may perform before being forced to "reload"(=refilling is ammo points)
+        int range; // An upper bound to the distance in a board this unit may attack if he spots an enemy unit.
+        int power; // the amount of hit points(or damage) the said unit will inflict to each of the enemy units in the map(in it's range), given its attack. 
+        Team team; // Specifies the team this unit belongs to.
+        CharacterType type; // Specifies what is the type of the character.
         int ammo_reload;//ammo to give you when reload
-        int max_steps;
-        int ammo_shooting;//ammo when you shoot,when you shoot ,shoot dont talk.
+        int max_steps; // represent an upper bound to the amount of steps a unit can walk in a single direction.
+        int ammo_shooting; // specifies the number of ammo points that will be reduced from calling "special attack" function.
+        // this number also specifies the minimal number of ammo points that allows a unit to perform its "special attack".
         Character(int health, int ammo, int range, int power, Team team,CharacterType type, int ammo_reload=0,int max_steps=0,int ammo_shooting=0);
     public:
-        ~Character() = default;
-        Character(const Character &hero) = default;
-        Character &operator=(const Character& hero) = default;
+        ~Character() = default; // as there are no memory allocation, no need for explicit deletion.
+        Character(const Character &hero) = default; // no explicit memory allocation in consturction implies no need for explicit copy constructor.
+        Character &operator=(const Character& hero) = default; // default operator= produced by compiler provides the same functionality.
         //generic functions
 
         /* function specialAttack- function to set the diffrent effect of attack for each character.
         @param source- the coordinate of the attacker.
-        @param dest- the coortinate to attack.
-        @param board - the character who attack.
+        @param dest- the coordinate to attack.
+        @param board - the board in which the attack performs it's special attack.
         */
-         virtual void specialAttack(const GridPoint& source,const GridPoint &dest,std::vector<std::vector<std::shared_ptr < Character>>>& board);
+         virtual void specialAttack(const GridPoint& source, const GridPoint &dest ,std::vector<std::vector<std::shared_ptr < Character>>>& board);
 
-         /* isValidMove - a virtual function that check if the charcter can move.
+         /* isValidMove - a virtual function that check if the charcter can perform a given move.
          @param source- the coordinate of the charcter.
          @param dest- the coortinate to move.
          @param board- the character to move.
@@ -53,30 +54,31 @@ namespace mtm {
 
          /* getUniqueAmmo- a virtual function to get for each character his unique num of ammo when reload.
          */
-          int getUniqueAmmo();
+          int getUniqueAmmo() const;
         //generic getters&&setters getters&setters.
 
         /* getHealth - function to get the health that the character has.
         */
-         int getHealth();
+         int getHealth() const;
 
          /* getMaxSteps- a function to get the max steps that the character can do.
          */
-          int getMaxSteps();
+          int getMaxSteps() const;
 
          /* updateHealth- a function to update health.
          @param damage- the amount to down when damage made.
-         -id damage is less then zero, it means healing
+
+         -if damage is less then zero, it means healing
          */
          void updateHealth(int damage);
 
          /* function getAmmo - return the ammo of the character.
          */
-         int getAmmo();
+         int getAmmo() const;
 
          /* getRange- function that return the range of the charcter.
          */
-         int getRange() ;
+         int getRange() const;
 
          /* updateAmmo - virtual function to update the ammo.
          @param ammo- the amount of ammo to add.
@@ -86,13 +88,13 @@ namespace mtm {
          /*
           * getPower- return the power of the charcter.
          */
-         int getPower();
+         int getPower() const;
 
          /*
          * getTeam- return the type of team.
          */
-         Team getTeam();
-        CharacterType getType();
+         Team getTeam() const;
+        CharacterType getType() const;
          virtual Character& clone()=0;
         //general functions that will be used within GameClass;
 
@@ -101,13 +103,13 @@ namespace mtm {
         *@param dest- the coordinate to attack.
         *@param - board - the player who attack
         */
-        void attack(const GridPoint& source,const GridPoint& dest,std::vector<std::vector<std::shared_ptr < Character>>>& board);
+        void attack(const GridPoint& source,const GridPoint& dest,std::vector<std::vector<std::shared_ptr <Character>>>& board);
 
         /* isSameTeam - a boolean function to check if the players in same team.
         @param character - the character to compare.
         @return true- if they in the same team, if not, return false.
         */
-        bool isSameTeam(Character& character);//we cannot pass abstarct cas
+        bool isSameTeam(const Character& character) const;//we cannot pass abstarct cas
 
 
         void move(std::vector<std::vector<std::shared_ptr < Character>>> board,GridPoint dest);
